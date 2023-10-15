567be24b550d8ff3444c0785af34dd5987fa4c14
Jim Cooling <jimcooling42@gmail.com>
2023-10-15 16:23:58 +0100

    Testing upload of folder

---
+++ RTOS book 2 preliminary exercise VSC /Software delay function.c
@@ -0,0 +1,18 @@
+int Count;
+
+void Delay(void);
+
+void main (void)
+{
+  Delay();
+;
+
+} /*end main */
+
+
+
+void Delay(void)
+{
+  int Count = 0;
+  for (Count=0; Count<=200000000; Count++);   
+}
